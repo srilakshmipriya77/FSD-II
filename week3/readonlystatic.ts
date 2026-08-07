5e54b2962328a6e5c9d7dbbbd12ed1d801fb10c4
srilakshmipriya77 <srilakshmipriya7777@gmail.com>
2026-08-07 10:29:57 +0530

    Update readonlystatic.ts

--- week3/readonlystatic.ts
+++ week3/readonlystatic.ts
@@ -1,5 +1,5 @@
 
-class BankAccount {
+class StateBankAccount {
     readonly accountNumber: number;
     accountHolder: string;
     balance: number;
@@ -12,12 +12,13 @@ class BankAccount {
         this.accountHolder = accountHolder;
         this.balance = balance;
 
-        BankAccount.totalAccounts++;
+        StateBankAccount.totalAccounts++;
 
-        console.log("Bank Name:", BankAccount.bankName);
+        console.log("Bank Name:", StateBankAccount.bankName);
     }
 
     display(): void {
+        console.log("-------------------------");
         console.log("Account Number:", this.accountNumber);
         console.log("Account Holder:", this.accountHolder);
         console.log("Balance:", this.balance);
@@ -25,22 +26,21 @@ class BankAccount {
     }
 
     static displayBankDetails(): void {
-        console.log("Bank Name:", BankAccount.bankName);
-        console.log("Total Accounts:", BankAccount.totalAccounts);
+        console.log("Bank Name:", StateBankAccount.bankName);
+        console.log("Total Accounts:", StateBankAccount.totalAccounts);
     }
 }
 
-const acc1 = new BankAccount(101, "Lahari", 50000);
-const acc2 = new BankAccount(102, "Sai", 75000);
+const acc1 = new StateBankAccount(101, "Lahari", 50000);
+const acc2 = new StateBankAccount(102, "Sai", 75000);
 
 acc1.display();
 acc2.display();
 
-BankAccount.displayBankDetails();
+StateBankAccount.displayBankDetails();
 
-// Testing readonly
 
-// acc1.accountNumber = 200;   // ❌ Error
+// acc1.accountNumber = 200;   Error
 
 acc1.balance = 60000;
 
@@ -48,5 +48,5 @@ console.log("Updated Balance:", acc1.balance);
 
 // Testing static properties
 
-console.log("Bank:", BankAccount.bankName);
-console.log("Accounts Created:", BankAccount.totalAccounts);
+console.log("Bank:", StateBankAccount.bankName);
+console.log("Accounts Created:", StateBankAccount.totalAccounts);
