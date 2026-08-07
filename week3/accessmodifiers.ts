d35a3d9afb0feedbf44fa55df43d9c181d2f70ab
srilakshmipriya77 <srilakshmipriya7777@gmail.com>
2026-08-07 10:13:46 +0530

    Update accessmodifiers.ts

--- week3/accessmodifiers.ts
+++ week3/accessmodifiers.ts
@@ -1,32 +1,43 @@
-
-class Employee1 {
-    public name: string;
-    private salary: number;
-    protected department: string;
-
-    constructor(name: string, salary: number, department: string) {
-        this.name = name;
-        this.salary = salary;
-        this.department = department;
+class BankAccount{
+    public accountNumber: string;
+    private balance: number;
+    private secretPin: string;
+    protected accountHolderName: string;
+    constructor(accountNumber: string, balance: number, secretPin: string, accountHolderName: string){
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+        this.secretPin=secretPin;
+        this.accountHolderName=accountHolderName;
     }
-
-    public display(): void {
-        console.log("Name:", this.name);
-        console.log("Salary:", this.salary);
-        console.log("Department:", this.department);
+    public displayAccountInfo(): void{
+        console.log("--------------------------------------------------");
+        console.log("Account Number: " + this.accountNumber);
+        console.log("Account Holder Name: " + this.accountHolderName);
+        console.log("Balance: " + this.balance);
+        console.log("--------------------------------------------------");
+    }
+    public withdrawMoney(pin: string, amount: number):void{
+        if(this.verifyPin(pin) && amount <= this.balance){
+            this.balance-= amount;
+            console.log("Money withdrawn successfully. New balance: " + this.balance);
+        }
+        else if(!this.verifyPin(pin)){
+             console.log("Incorrect PIN. Transaction failed.");
+             return;
+        }
+        else{
+            console.log("Insufficient funds. Transaction failed.");
+            return;
+        }
+    }
+    private verifyPin(pin:string): boolean{
+        return this.secretPin === pin;
     }
 }
 
-const emp = new Employee1("Lahari", 50000, "AIML");
+const account1=new BankAccount("101", 5000,"1234", "John");
+account1.displayAccountInfo();
 
-// Public member
-console.log(emp.name);
-
-// Public method
-emp.display();
-
-// Private member
-// console.log(emp.salary);      // ❌ Error
-
-// Protected member
-// console.log(emp.department);  // ❌ Error
+account1.withdrawMoney("1234",1000);
+account1.withdrawMoney("1234",6000);
+account1.withdrawMoney("4321",1000);
